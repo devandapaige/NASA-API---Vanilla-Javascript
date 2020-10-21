a4a44bdb76620420e4d8902ccc7bfc334065bb5b
Amanda Nelson <amanda.gaylord@gmail.com>
2020-10-21 19:10:27 -0400

    MainContent.js and PhotoOTD.js has been updated and is displaying. Axios was used on PhotoOTD.js in a UseEffect to only render the API data once, then was passed into state to use the API data. - MVP complete

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
 import PhotoOTD from "./PhotoOTD";
-const mainBody = document.createElement("div");
-mainBody.style = "width: 80%;";
-function MainContent() {
+
+function MainContent(props) {
   return (
-    <mainBody>
+    <div className="main">
+      <h2 className="day">Today: </h2>
       <PhotoOTD />
-    </mainBody>
+    </div>
   );
 }
 export default MainContent;
--- src/PhotoOTD.js
+++ src/PhotoOTD.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const photo = document.createElement("img");
-const titleDiv = document.createElement("div");
-photo.alt = "photo of the day";
-titleDiv.textContent = "Sample Text";
-
 function PhotoOfTheDay() {
-  return { photo }, { titleDiv };
+  let [data, setData] = useState({});
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=dL6iTE6RXGcbhm5tAH4POoh8Fo9THk7w14gSLet5"
+      )
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => console.log(err.error));
+  }, []);
+  console.log(data);
+  return (
+    <>
+      <img className="photoOTD" src={data.hdurl} alt={data.title}></img>
+      <h4 className="photoTitle">{data.title}</h4>
+      <p className="copyright">Copyright: {data.copyright}</p>
+      <p className="photoInfo">{data.explanation}</p>
+    </>
+  );
 }
 export default PhotoOfTheDay;
