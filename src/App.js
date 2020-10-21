528bdbf8349d971b680b8cde4bfbd6bafa8a0944
Amanda Nelson <amanda.gaylord@gmail.com>
2020-10-21 18:09:28 -0400

    Footer.js styled and appearing properly

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Footer from "./Footer"
 function App() {
   return (
     <div>
       <Header />
+      <Footer />
       </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Footer() {
+  return (
+    <footer className="footer">
+<p>Coded by <a href="https://github.com/devandapaige">Amanda Nelson 🐈</a></p>
+<p>2020</p>
+    </footer>
+  );
+}
+export default Footer;
