34e78d437a1c257807166e54d88baf3851fe5535
Amanda Nelson <amanda.gaylord@gmail.com>
2020-10-21 18:17:39 -0400

    aria label added to emoji in footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,8 +2,16 @@ import React from "react";
 function Footer() {
   return (
     <footer className="footer">
-<p>Coded by <a href="https://github.com/devandapaige">Amanda Nelson 🐈</a></p>
-<p>2020</p>
+      <p>
+        Coded by{" "}
+        <a href="https://github.com/devandapaige">
+          Amanda Nelson{" "}
+          <span role="img" aria-label="cat standing emoji">
+            🐈
+          </span>
+        </a>
+      </p>
+      <p>2020</p>
     </footer>
   );
 }
