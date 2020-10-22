4e360590794b7735aa76272fd35b052721901fad
Amanda Nelson <amanda.gaylord@gmail.com>
2020-10-22 13:40:55 -0400

    Header.js styled with styled-components

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,33 @@
 //Nasa logo:https://www.nasa.gov/sites/all/themes/custom/nasatwo/images/nasa-logo.svg
+//Nasa blue: #0B3D91 / Nasa red: #FC3D21 / contrast light blue: #5D7FB9*//
 import React from "react";
+import styled from "styled-components";
+styled.body`
+  margin: 0;
+  background-color: black;
+  color: white;
+`;
+const headerStyle = styled.header`
+  display: flex;
+  justify-content: space-between;
+  border-bottom: solid 3px #5d7fb9;
+`;
+const siteName = styled.header`
+  padding: 10px;
+`;
+const logo = styled.img`
+  width: 15%;
+  padding: 10px;
+`;
 function Header() {
   return (
-    <header className="header">
-      <h1 className="siteName">NASA Photo of the Day</h1>
-      <img
-        className="logo"
+    <headerStyle>
+      <siteName>NASA Photo of the Day</siteName>
+      <logo
         src="https://www.nasa.gov/sites/all/themes/custom/nasatwo/images/nasa-logo.svg"
         alt="NASA logo"
       />
-    </header>
+    </headerStyle>
   );
 }
 export default Header;
